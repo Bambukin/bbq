504693ea15c246f1c6c042b0642d494dd0d69f17
Vadim <vadimsorella@gmail.com>
2023-04-27 01:21:20 +0600

    add association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :events
+
+  validates :name, presence: true, length: {maximum: 35}
+  validates :email, presence: true, length: {maximum: 255}, email: true, uniqueness: true
 end
