be8d2babbb86f44e98527250ef80d5ec5dac5f8c
Vadim <vadimsorella@gmail.com>
2023-05-12 23:23:43 +0600

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :events, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :photos, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
   has_many :photos, dependent: :destroy
   has_one_attached :avatar do |attachable|
@@ -11,15 +10,13 @@ class User < ApplicationRecord
   end
 
   validates :avatar, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'] }
+  validates :avatar, presence: true, on: :update
   validates :name, presence: true, length: {maximum: 35}
-  validates :avatar, presence: true
 
   before_validation :set_name, on: :create
   before_validation :downcase_email
 
   after_commit :link_subscriptions, on: :create
-
-  mount_uploader :avatar, AvatarUploader
 
   private
 
