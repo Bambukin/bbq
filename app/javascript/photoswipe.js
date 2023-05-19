c426556e3932e358895e8fd4f259b3f4278f569a
Vadim <vadimsorella@gmail.com>
2023-05-20 02:59:02 +0600

    use turbo

--- app/javascript/photoswipe.js
+++ app/javascript/photoswipe.js
@@ -1,34 +1,41 @@
 import PhotoSwipeLightbox from 'photoswipe/lightbox';
-const options = {
-    gallery:'#my-gallery',
-    children:'.pswp-gallery__item',
-    pswpModule: () => import('photoswipe')
-};
-const lightbox = new PhotoSwipeLightbox(options);
-lightbox.on('uiRegister', function() {
-    lightbox.pswp.ui.registerElement({
+
+document.addEventListener('turbo:load', function (event) {
+  const gallery = document.getElementById('my-gallery');
+
+  if (gallery && !gallery.hasAttribute('data-transformed')) {
+    const options = {
+      gallery: '#my-gallery',
+      children: '.pswp-gallery__item',
+      pswpModule: () => import('photoswipe')
+    };
+
+    const lightbox = new PhotoSwipeLightbox(options);
+    lightbox.on('uiRegister', function () {
+      lightbox.pswp.ui.registerElement({
         name: 'custom-caption',
         order: 9,
         isButton: false,
         appendTo: 'root',
         html: 'Caption text',
         onInit: (el, pswp) => {
-            lightbox.pswp.on('change', () => {
-                const currSlideElement = lightbox.pswp.currSlide.data.element;
-                let captionHTML = '';
-                if (currSlideElement) {
-                    const hiddenCaption = currSlideElement.querySelector('.hidden-caption-content');
-                    if (hiddenCaption) {
-                        // get caption from element with class hidden-caption-content
-                        captionHTML = hiddenCaption.innerHTML;
-                    } else {
-                        // get caption from alt attribute
-                        captionHTML = currSlideElement.querySelector('img').getAttribute('alt');
-                    }
-                }
-                el.innerHTML = captionHTML || '';
-            });
+          lightbox.pswp.on('change', () => {
+            const currSlideElement = lightbox.pswp.currSlide.data.element;
+            let captionHTML = '';
+            if (currSlideElement) {
+              const hiddenCaption = currSlideElement.querySelector('.hidden-caption-content');
+              if (hiddenCaption) {
+                captionHTML = hiddenCaption.innerHTML;
+              } else {
+                captionHTML = currSlideElement.querySelector('img').getAttribute('alt');
+              }
+            }
+            el.innerHTML = captionHTML || '';
+          });
         }
+      });
     });
+    lightbox.init();
+    gallery.setAttribute('data-transformed', true);
+  }
 });
-lightbox.init();
