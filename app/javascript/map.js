a3fd9c7ba23efcace698c562c25efbf2c8f6cb30
Vadim <vadimsorella@gmail.com>
2023-05-22 15:51:21 +0600

    add turbo reload

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,40 +1,44 @@
 document.addEventListener('turbo:load', function (event) {
   const mapElement = document.getElementById('map');
 
-  if (mapElement && !mapElement.hasAttribute('data-transformed')) {
-    ymaps.ready(init);
 
-    function init() {
-      const address = mapElement.getAttribute('data-address');
+  if (!mapElement || mapElement.hasAttribute('data-transformed')) {
+    return;
+  }
 
-      const myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 10
-      });
+  ymaps.ready(init);
 
-      const myGeocoder = ymaps.geocode(address);
+  function init() {
+    const address = mapElement.getAttribute('data-address');
 
-      myGeocoder.then(
-        function (res) {
-          const coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+    const myMap = new ymaps.Map("map", {
+      center: [55.76, 37.64],
+      zoom: 10,
+      controls: []
+    });
 
-          myMap.geoObjects.add(
-            new ymaps.Placemark(
-              coordinates,
-              {iconContent: address},
-              {preset: 'islands#blueStretchyIcon'}
-            )
-          );
+    const myGeocoder = ymaps.geocode(address);
 
-          myMap.setCenter(coordinates);
-          myMap.setZoom(15);
-        },
-        function (err) {
-          alert('Ошибка при определении местоположения');
-        }
-      );
+    myGeocoder.then(
+      function (res) {
+        const coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
-      mapElement.setAttribute('data-transformed', true);
-    }
+        myMap.geoObjects.add(
+          new ymaps.Placemark(
+            coordinates,
+            {iconContent: address},
+            {preset: 'islands#blueStretchyIcon'}
+          )
+        );
+
+        myMap.setCenter(coordinates);
+        myMap.setZoom(15);
+      },
+      function (err) {
+        alert('Ошибка при определении местоположения');
+      }
+    );
+
+    mapElement.setAttribute('data-transformed', true);
   }
 });
--- app/javascript/photoswipe.js
+++ app/javascript/photoswipe.js
@@ -3,39 +3,37 @@ import PhotoSwipeLightbox from 'photoswipe/lightbox';
 document.addEventListener('turbo:load', function (event) {
   const gallery = document.getElementById('my-gallery');
 
-  if (gallery && !gallery.hasAttribute('data-transformed')) {
-    const options = {
-      gallery: '#my-gallery',
-      children: '.pswp-gallery__item',
-      pswpModule: () => import('photoswipe')
-    };
+  const options = {
+    gallery: '#my-gallery',
+    children: '.pswp-gallery__item',
+    pswpModule: () => import('photoswipe')
+  };
 
-    const lightbox = new PhotoSwipeLightbox(options);
-    lightbox.on('uiRegister', function () {
-      lightbox.pswp.ui.registerElement({
-        name: 'custom-caption',
-        order: 9,
-        isButton: false,
-        appendTo: 'root',
-        html: 'Caption text',
-        onInit: (el, pswp) => {
-          lightbox.pswp.on('change', () => {
-            const currSlideElement = lightbox.pswp.currSlide.data.element;
-            let captionHTML = '';
-            if (currSlideElement) {
-              const hiddenCaption = currSlideElement.querySelector('.hidden-caption-content');
-              if (hiddenCaption) {
-                captionHTML = hiddenCaption.innerHTML;
-              } else {
-                captionHTML = currSlideElement.querySelector('img').getAttribute('alt');
-              }
+  const lightbox = new PhotoSwipeLightbox(options);
+  lightbox.on('uiRegister', function () {
+    lightbox.pswp.ui.registerElement({
+      name: 'custom-caption',
+      order: 9,
+      isButton: false,
+      appendTo: 'root',
+      html: 'Caption text',
+      onInit: (el, pswp) => {
+        lightbox.pswp.on('change', () => {
+          const currSlideElement = lightbox.pswp.currSlide.data.element;
+          let captionHTML = '';
+          if (currSlideElement) {
+            const hiddenCaption = currSlideElement.querySelector('.hidden-caption-content');
+            if (hiddenCaption) {
+              captionHTML = hiddenCaption.innerHTML;
+            } else {
+              captionHTML = currSlideElement.querySelector('img').getAttribute('alt');
             }
-            el.innerHTML = captionHTML || '';
-          });
-        }
-      });
+          }
+          el.innerHTML = captionHTML || '';
+        });
+      }
     });
-    lightbox.init();
-    gallery.setAttribute('data-transformed', true);
-  }
+  });
+  lightbox.init();
+  gallery.setAttribute('data-transformed', true);
 });
